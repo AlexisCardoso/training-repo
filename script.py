e6b2a0ef262bc80272f0cdde518716dd66cacf1e
U-TMAIS\Alexis.Duarte <Alexis.Duarte@DT-CN-20.tmais.cv>
2020-04-16 08:49:47 -0100

    15/04/2020

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 #!/bin/python
 fname = raw_input("What is your name: ")
 print("Hi " + fname)
-print("Press enter")
+
+
+a = 1
+if fname=="Alexis":
+  fpass = raw_input("What is your password: ")
+  if fpass=="Passw0rd":
+    print("You may enter")
+  else:
+    print("Login failed")
+else:
+  print("You are not allowed!")
